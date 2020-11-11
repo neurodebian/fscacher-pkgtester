1f3f633f2ae340ab66f8f6e2598923f0e20bd9aa
John T. Wodder II <git@varonathe.org>
2020-11-11 10:28:14 -0500

    Use versioneer

--- setup.py
+++ setup.py
@@ -1,2 +1,15 @@
+import os.path
+import sys
 from setuptools import setup
-setup()
+
+# This is needed for versioneer to be importable when building with PEP 517.
+# See <https://github.com/warner/python-versioneer/issues/193> and links
+# therein for more information.
+sys.path.append(os.path.dirname(__file__))
+
+import versioneer
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
--- src/pyfscacher/__init__.py
+++ src/pyfscacher/__init__.py
@@ -4,7 +4,9 @@ Caching results of operations on heavy file trees
 Visit <https://github.com/con/pyfscacher> for more information.
 """
 
-__version__      = '0.1.0.dev1'
+from ._version import get_versions
+
+__version__      = get_versions()['version']
 __author__       = 'Center for Open Neuroscience'
 __author_email__ = 'debian@onerussian.com'
 __license__      = 'MIT'
