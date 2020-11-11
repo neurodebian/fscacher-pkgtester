77541d929b54282c114ce7eea0bb67e9091284ee
John T. Wodder II <git@varonathe.org>
2020-11-11 10:11:48 -0500

    Packaging

---
+++ src/pyfscacher/__init__.py
@@ -0,0 +1,11 @@
+"""
+Caching results of operations on heavy file trees
+
+Visit <https://github.com/con/pyfscacher> for more information.
+"""
+
+__version__      = '0.1.0.dev1'
+__author__       = 'Center for Open Neuroscience'
+__author_email__ = 'debian@onerussian.com'
+__license__      = 'MIT'
+__url__          = 'https://github.com/con/pyfscacher'
