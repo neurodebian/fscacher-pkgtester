2c71a4908528ff7202a3334978af1d8b3822d158
John T. Wodder II <git@varonathe.org>
2020-11-17 11:30:41 -0500

    Run benchmarks on multiple file systems

--- benchmarks/cache.py
+++ benchmarks/cache.py
@@ -1,6 +1,7 @@
 from hashlib import sha256
 import os
 from pathlib import Path
+import shutil
 import random
 from time import time
 from uuid import uuid4
@@ -35,23 +36,24 @@ class TimeFile:
 
 
 class TimeDirectoryFlat:
-    FILES = 100
-    param_names = ["n", "control"]
-    params = ([10, 100, 1000], ["", "ignore"])
+    LAYOUT = (100,)
 
-    def setup_cache(self):
-        base_time = time()
-        for x in range(self.FILES):
-            f = Path(f"f{x}")
-            f.write_bytes(b"\0" * random.randint(1, 1024))
-            t = base_time - x
-            os.utime(f, (t, t))
+    param_names = ["n", "control", "tmpdir"]
+    params = (
+        [10, 100, 1000],
+        ["", "ignore"],
+        os.environ.get("FSCACHER_BENCH_TMPDIRS", ".").split(":"),
+    )
 
-    def setup(self, n, control):
+    def setup(self, n, control, tmpdir):
+        cache_id = str(uuid4())
         with envset("FSCACHER_CACHE", control):
-            self.cache = PersistentCache(name=str(uuid4()))
+            self.cache = PersistentCache(name=cache_id)
+        self.dir = Path(tmpdir, cache_id)
+        self.dir.mkdir()
+        create_tree(self.dir, self.LAYOUT)
 
-    def time_directory(self, n, control):
+    def time_directory(self, n, control, tmpdir):
         @self.cache.memoize_path
         def dirsize(path):
             total_size = 0
@@ -64,30 +66,33 @@ class TimeDirectoryFlat:
             return total_size
 
         for _ in range(n):
-            dirsize(os.curdir)
+            dirsize(str(self.dir))
 
-    def teardown(self, n, control):
+    def teardown(self, *args, **kwargs):
         self.cache.clear()
+        shutil.rmtree(self.dir)
 
 
 class TimeDirectoryDeep(TimeDirectoryFlat):
-    def setup_cache(self):
-        base_time = time()
-        layout = (3, 3, 3, 3)
-        dirs = [Path()]
-        for i, width in enumerate(layout):
-            if i < len(layout) - 1:
-                dirs2 = []
-                for d in dirs:
-                    for x in range(width):
-                        d2 = d / f"d{x}"
-                        d2.mkdir()
-                        dirs2.append(d2)
-                dirs = dirs2
-            else:
-                for j, d in enumerate(dirs):
-                    for x in range(width):
-                        f = d / f"f{x}.dat"
-                        f.write_bytes(b"\0" * random.randint(1, 1024))
-                        t = base_time - x - j * width
-                        os.utime(f, (t, t))
+    LAYOUT = (3, 3, 3, 3)
+
+
+def create_tree(root, layout):
+    base_time = time()
+    dirs = [Path(root)]
+    for i, width in enumerate(layout):
+        if i < len(layout) - 1:
+            dirs2 = []
+            for d in dirs:
+                for x in range(width):
+                    d2 = d / f"d{x}"
+                    d2.mkdir()
+                    dirs2.append(d2)
+            dirs = dirs2
+        else:
+            for j, d in enumerate(dirs):
+                for x in range(width):
+                    f = d / f"f{x}.dat"
+                    f.write_bytes(b"\0" * random.randint(1, 1024))
+                    t = base_time - x - j * width
+                    os.utime(f, (t, t))
